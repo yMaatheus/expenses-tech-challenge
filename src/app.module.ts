94b6d4c62403f21852c2423ceb38a8b85b15b88a
Matheus <ymatheus.h@gmail.com>
2025-05-29 03:26:00 -0300

    refactor: remove unused AppController, AppService, and related tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from '@database/database.module';
+import { ExpenseModule } from '@modules/expense/expense.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, ExpenseModule],
 })
 export class AppModule {}
